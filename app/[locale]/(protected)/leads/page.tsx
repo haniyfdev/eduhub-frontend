b3fc3f63f3b8a66e6c8a177b7fb06b79284d1554
haniyfdev <haniyfmuhsin@gmail.com>
2026-05-14 14:44:37 +0500

    Menimcha tugadi

--- app/[locale]/(protected)/leads/page.tsx
+++ app/[locale]/(protected)/leads/page.tsx
@@ -269,7 +269,7 @@ export default function LeadsPage() {
         <div className="relative flex-1 max-w-xs">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
           <input
-            type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Qidirish..."
+            type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Ism yoki familiya..."
             className="w-full pl-9 pr-3 py-2 border border-gray-300 rounded text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>

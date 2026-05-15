88309be8c93b46a4227d428d02a7cd7fdb5e97b9
haniyfdev <haniyfmuhsin@gmail.com>
2026-05-15 14:14:28 +0500

    fix attendance

--- app/[locale]/(protected)/attendance/page.tsx
+++ app/[locale]/(protected)/attendance/page.tsx
@@ -102,13 +102,18 @@ export default function AttendancePage() {
     }));
   }
 
-  function toggleSelect(id: string) {
-    setSelected(prev => {
-      const next = new Set(prev);
-      next.has(id) ? next.delete(id) : next.add(id);
-      return next;
-    });
-  }
+function toggleSelect(id: string) {
+  setSelected(prev => {
+    const next = new Set(prev);
+    // Ternar operator o'rniga if/else ishlatamiz
+    if (next.has(id)) {
+      next.delete(id);
+    } else {
+      next.add(id);
+    }
+    return next;
+  });
+}
 
   async function openCalendar(row: AttendanceSummaryRow) {
     setCalendar(row);
